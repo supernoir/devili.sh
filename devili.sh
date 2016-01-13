62d4e43cd6cb0b341d19d4f416b7acd1af02a106
supernoir <kawohl.bentodesign@gmail.com>
2016-01-13 15:43:39 +0100

    Initial Commit

---
+++ devili.sh
@@ -0,0 +1,8 @@
+pwd;
+echo "Let's get MEAN. Muaaaaahaha!";
+echo " - - - - - - - - - - - -";
+echo " ";
+echo "Have Bower install angular."
+bower install angular;
+bower angular -v;
+echo "Angular: Devilishly done."
